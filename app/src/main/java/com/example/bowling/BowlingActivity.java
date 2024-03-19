3d98b7e8adb7552a5e71e3ff539571de3b672f6f
ida.olsson.1361@student.lu.se <90462101+ida02olsson@users.noreply.github.com>
2024-03-19 13:41:55 +0100

    Lagt till käglor, bowlingklot och att en kägla kan trilla

--- app/src/main/java/com/example/bowling/BowlingActivity.java
+++ app/src/main/java/com/example/bowling/BowlingActivity.java
@@ -6,8 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class BowlingActivity extends AppCompatActivity {
+
+    private ImageView kagla1;
+    private ImageView kagla2;
+    private ImageView kagla3;
+    private ImageView kagla4;
+    private ImageView kagla5;
+    private ImageView kagla6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +24,32 @@ public class BowlingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bowling);
 
 
-        Button returnButton = findViewById(R.id.returnButton);
+        ImageButton returnButton = findViewById(R.id.returnButton);
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(BowlingActivity.this, MainActivity.class));
             }
+        });
+
+        kagla6 = (ImageView) findViewById(R.id.kagla6);
+        Button bowlingButton = findViewById(R.id.bowlingButton);
+
+        bowlingButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick (View v){
+                // Calculate the final rotation angle (90 degrees to the right)
+                final float finalRotation = kagla6.getRotation() + 90f;
+                float translationYDistance = kagla6.getHeight() * 0.2f;
+
+                // Start the animation
+                kagla6.animate()
+                        .setDuration(300)
+                        .rotation(finalRotation)
+                        .translationYBy(translationYDistance)
+                        .start();
+                }
+
         });
     }
 }
